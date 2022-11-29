90602916c8a219227f6c78b14fdefa19c8ece3ee
steve <stephen@stephenmucheru79@gmail.com>
2022-11-29 09:18:58 +0300

    all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import PostsList from './feature/api/posts/PostsList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    <h2 className="mb-5">React RTK Query CRUD Operations Example</h2>
+    <PostsList />
+  </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { apiSlice } from './feature/api/apiSlice';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ApiProvider api={apiSlice}>
     <App />
+    </ApiProvider>
   </React.StrictMode>
 );
 
